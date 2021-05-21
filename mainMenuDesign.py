f711b6fdc4558201b6c10991be1284478db5d872
17028 <81329807+17028@users.noreply.github.com>
2021-05-21 13:26:56 +1200

    Re-uploading correct version
    Uploaded incorrect version earlier

--- mainMenuDesign.py
+++ mainMenuDesign.py
@@ -10,21 +10,21 @@ BGCOLOUR = "light grey"
 class Title_screen:
     def __init__(self):
         Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .pack()
-        Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",command=raiseframe(f2)) .pack()
+        Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",command="") .pack()
         Label(f1,text="",bg=BGCOLOUR).pack()
         Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",command=quit) .pack()
 
 def main():
-    global f1,f2,root
+    global f1,root
     root = Tk()
     root.title("L2 Calculus Program")
     root.geometry("1280x720")
     root.resizable(False, False)
     f1 = Frame(root,bg=BGCOLOUR)
-    f2 = Frame(root,bg=BGCOLOUR)
     root.configure(bg=BGCOLOUR)
-    f1.pack(side='top',expand='true',fill="y")
+    f1.pack(expand=TRUE)
     raiseframe(f1)
+
 main()
 Title_screen()
 mainloop()
