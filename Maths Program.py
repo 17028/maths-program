def7cae9c94da63a189155de65bf2827825e5093
17028 <81329807+17028@users.noreply.github.com>
2021-05-26 10:19:34 +1200

    26/05 - Finished development of main menu
    Finished both coding and testing of main menu system

--- Maths Program.py
+++ Maths Program.py
@@ -1,22 +1,24 @@
 from tkinter import *
 
-def raiseframe(frame):
-    frame.tkraise()
-
+BUTTONHEIGHT = 2
+BUTTONWIDTH = 15
 TITLEFONT = "Consolas",100
 BUTTONFONT = "Consolas",20
 BGCOLOUR = "light grey"
 
+def raiseframe(frame):
+    frame.tkraise()
+
 class Title_screen():
     def __init__(self):
-        Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .pack()
-        Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",command=lambda:raiseframe(f2)) .pack()
-        Label(f1,text="",bg=BGCOLOUR) .pack()
-        Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",command=quit) .pack()
+        Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .grid(pady=100)
+        Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=lambda:raiseframe(f2)) .grid()
+        Label(f1,text="",bg=BGCOLOUR) .grid()
+        Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=quit) .grid(pady=30)
 
 class Selection_screen():
     def __init__(self):
-        Label(f2,text="whee",font=TITLEFONT) .grid(column=0,row=0)
+        Label(f2,text="this is page 2",font=TITLEFONT) .grid(column=0,row=0)
 
 
 def main():
@@ -25,14 +27,15 @@ def main():
     root.title("L2 Calculus Program")
     root.geometry("1280x720")
     root.resizable(False, False)
+    root.configure(bg=BGCOLOUR)
     stacking_frame = Frame()
     stacking_frame.pack()
     f1 = Frame(master=stacking_frame,bg=BGCOLOUR)
     f2 = Frame(master=stacking_frame,bg=BGCOLOUR)
-    root.configure(bg=BGCOLOUR)
+    for frame in (f1,f2):
+        frame.grid(column=0,row=0,sticky='news')
     f1.grid(column=0,row=0,sticky='news')
     f1.columnconfigure(0,weight=1)
-    #f1.pack(expand=TRUE)
     f1.rowconfigure(0,weight=1)
     f2.grid(column=0,row=0,sticky='news')
     f2.columnconfigure(0,weight=1)
