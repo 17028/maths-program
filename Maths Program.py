a7a7f837b5ab480e0c59a3d34862b4964211f904
17028 <81329807+17028@users.noreply.github.com>
2021-05-28 12:24:39 +1200

    28/05 - Updating the import method
    No other changes - only import method is changed

--- Maths Program.py
+++ Maths Program.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 
 BUTTONHEIGHT = 2
 BUTTONWIDTH = 15
@@ -11,27 +11,27 @@ def raiseframe(frame):
 
 class Title_screen():
     def __init__(self):
-        Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .grid(pady=100)
-        Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=lambda:raiseframe(f2)) .grid()
-        Label(f1,text="",bg=BGCOLOUR) .grid()
-        Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=quit) .grid(pady=30)
+        tk.Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .grid(pady=100)
+        tk.Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=lambda:raiseframe(f2)) .grid()
+        tk.Label(f1,text="",bg=BGCOLOUR) .grid()
+        tk.Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=quit) .grid(pady=30)
 
 class Selection_screen():
     def __init__(self):
-        Label(f2,text="this is page 2",font=TITLEFONT) .grid(column=0,row=0)
+        tk.Label(f2,text="this is page 2",font=TITLEFONT) .grid(column=0,row=0)
 
 
 def main():
     global f1,f2,root
-    root = Tk()
+    root = tk.Tk()
     root.title("L2 Calculus Program")
     root.geometry("1280x720")
     root.resizable(False, False)
     root.configure(bg=BGCOLOUR)
-    stacking_frame = Frame()
+    stacking_frame = tk.Frame()
     stacking_frame.pack()
-    f1 = Frame(master=stacking_frame,bg=BGCOLOUR)
-    f2 = Frame(master=stacking_frame,bg=BGCOLOUR)
+    f1 = tk.Frame(master=stacking_frame,bg=BGCOLOUR)
+    f2 = tk.Frame(master=stacking_frame,bg=BGCOLOUR)
     for frame in (f1,f2):
         frame.grid(column=0,row=0,sticky='news')
     f1.grid(column=0,row=0,sticky='news')
@@ -45,6 +45,5 @@ def main():
     f1.tkraise()
     root.mainloop()
 
-
-main()
-
+if __name__ == '__main__':
+    main()
