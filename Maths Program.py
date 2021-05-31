58a0a4febd66f0e635070c92b0b01a8470b4212c
17028 <81329807+17028@users.noreply.github.com>
2021-05-31 13:35:55 +1200

    31/05 - Rewrite of the page-switching system
    Foundational rewrite, changing the program from a stacked frame-raising approach to a create-and-destroy approach that is more in line with coding convention.

--- Maths Program.py
+++ Maths Program.py
@@ -16,53 +16,51 @@ DIFFBG = "spring green3"
 INTBG = "royal blue2"
 SMALLBUTTONFONT = "Arial"
 
-def raiseframe(frame):
-    frame.tkraise()
-
-class Title_screen():
+class Maths(tk.Tk):
     def __init__(self):
-        tk.Label(f1,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .grid(pady=100)
-        tk.Button(f1,text="Start",font=BUTTONFONT,bg="spring green3",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=lambda:raiseframe(f2)) .grid()
-        tk.Label(f1,text="",bg=BGCOLOUR) .grid()
-        tk.Button(f1,text="Quit",font=BUTTONFONT,bg="orange red2",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=quit) .grid(pady=30)
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(titleScreen)
+        self.title("L2 Calculus Program")
 
-class Selection_screen():
-    def __init__(self):
-        tk.Label(f2,text="Lessons",font=TITLEFONT,bg=BGCOLOUR) .grid(column=1,row=0)
-        tk.Button(f2,text='Differentiation;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=1,padx=BUTTONGAPX,pady=BUTTONGAPY)
-        tk.Button(f2,text='Differentiation;Gradients',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=1,padx=BUTTONGAPX)
-        tk.Button(f2,text='Differentiation;Tangents',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=2,row=1,padx=BUTTONGAPX)
-        tk.Button(f2,text='Differentiation;Δ Graident',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=2,pady=BUTTONGAPY)
-        tk.Button(f2,text='Differentiation;Max/Min',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=2)
-        tk.Button(f2,text='Integration;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=2,row=2)
-        tk.Button(f2,text='Integration;Functions',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=0,row=3)
-        tk.Button(f2,text='Integration;Kinematics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= INTBG,font = SMALLBUTTONFONT) .grid(column=1,row=3,pady=BUTTONGAPY)
-        tk.Button(f2,text='Final Quiz',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= "chocolate2",font = SMALLBUTTONFONT) .grid(column=2,row=3)
-        tk.Button(f2,text='Back',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg="orange red2",font = SMALLBUTTONFONT) .grid(column=1,row=4,pady=BACKBUTTONGAPY)
+    def switch_frame(self,frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class titleScreen(tk.Frame):
+    def __init__(self,master):
+        tk.Frame.__init__(self,master)
+        self.config(bg=BGCOLOUR)
+        tk.Label(self,text="Calculus",font=TITLEFONT,bg=BGCOLOUR) .grid(pady=100)
+        tk.Button(self,text="Start",font=BUTTONFONT,bg="spring green3",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=lambda:master.switch_frame(mainMenu)) .grid()
+        tk.Label(self,text="",bg=BGCOLOUR) .grid()
+        tk.Button(self,text="Quit",font=BUTTONFONT,bg="orange red2",width = BUTTONWIDTH, height = BUTTONHEIGHT,command=quit) .grid(pady=30)
+
+class mainMenu(tk.Frame):
+    def __init__(self,master):
+        tk.Frame.__init__(self,master)
+        self.config(bg=BGCOLOUR)
+        tk.Label(self,text="Lessons",font=TITLEFONT,bg=BGCOLOUR) .grid(column=1,row=0)
+        tk.Button(self,text='Differentiation;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=1,padx=BUTTONGAPX,pady=BUTTONGAPY)
+        tk.Button(self,text='Differentiation;Gradients',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=1,padx=BUTTONGAPX)
+        tk.Button(self,text='Differentiation;Tangents',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=2,row=1,padx=BUTTONGAPX)
+        tk.Button(self,text='Differentiation;Δ Graident',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=2,pady=BUTTONGAPY)
+        tk.Button(self,text='Differentiation;Max/Min',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=2)
+        tk.Button(self,text='Integration;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=2,row=2)
+        tk.Button(self,text='Integration;Functions',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=0,row=3)
+        tk.Button(self,text='Integration;Kinematics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= INTBG,font = SMALLBUTTONFONT) .grid(column=1,row=3,pady=BUTTONGAPY)
+        tk.Button(self,text='Final Quiz',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= "chocolate2",font = SMALLBUTTONFONT) .grid(column=2,row=3)
+        tk.Button(self,text='Back',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command=lambda:master.switch_frame(titleScreen),bg="orange red2",font = SMALLBUTTONFONT) .grid(column=1,row=4,pady=BACKBUTTONGAPY)
 
 def main():
-    global f1,f2,root
-    root = tk.Tk()
-    root.title("L2 Calculus Program")
-    root.geometry("1280x720")
-    root.resizable(False, False)
-    root.configure(bg=BGCOLOUR)
-    stacking_frame = tk.Frame()
-    stacking_frame.pack()
-    f1 = tk.Frame(master=stacking_frame,bg=BGCOLOUR)
-    f2 = tk.Frame(master=stacking_frame,bg=BGCOLOUR)
-    for frame in (f1,f2):
-        frame.grid(column=0,row=0,sticky='news')
-    f1.grid(column=0,row=0,sticky='news')
-    f1.columnconfigure(0,weight=1)
-    f1.rowconfigure(0,weight=1)
-    f2.grid(column=0,row=0,sticky='news')
-    f2.columnconfigure(0,weight=1)
-    f2.rowconfigure(0,weight=1)
-    Title_screen()
-    Selection_screen()
-    f1.tkraise()
-    root.mainloop()
+    app = Maths()
+    app.geometry("1280x720")
+    app.config(bg=BGCOLOUR)
+    app.resizable(False, False)
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
