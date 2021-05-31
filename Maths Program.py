631d62a95234619aaaf3d40b681cf956709d4660
17028 <81329807+17028@users.noreply.github.com>
2021-05-31 13:13:01 +1200

    31/05 - Integration of lesson select screen
    Save before attempting to integrate a better page switching method

--- Maths Program.py
+++ Maths Program.py
@@ -3,8 +3,18 @@ import tkinter as tk
 BUTTONHEIGHT = 2
 BUTTONWIDTH = 15
 TITLEFONT = "Consolas",100
-BUTTONFONT = "Consolas",20
+BUTTONFONT = "Arial",20
 BGCOLOUR = "light grey"
+
+#Selection Screen Constants
+BUTTONGAPX = 50
+BUTTONGAPY = 20
+BACKBUTTONGAPY = 40
+SSBUTTONWIDTH = 20
+SSBUTTONHEIGHT = 4
+DIFFBG = "spring green3"
+INTBG = "royal blue2"
+SMALLBUTTONFONT = "Arial"
 
 def raiseframe(frame):
     frame.tkraise()
@@ -18,8 +28,17 @@ class Title_screen():
 
 class Selection_screen():
     def __init__(self):
-        tk.Label(f2,text="this is page 2",font=TITLEFONT) .grid(column=0,row=0)
-
+        tk.Label(f2,text="Lessons",font=TITLEFONT,bg=BGCOLOUR) .grid(column=1,row=0)
+        tk.Button(f2,text='Differentiation;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=1,padx=BUTTONGAPX,pady=BUTTONGAPY)
+        tk.Button(f2,text='Differentiation;Gradients',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=1,padx=BUTTONGAPX)
+        tk.Button(f2,text='Differentiation;Tangents',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=2,row=1,padx=BUTTONGAPX)
+        tk.Button(f2,text='Differentiation;Δ Graident',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=0,row=2,pady=BUTTONGAPY)
+        tk.Button(f2,text='Differentiation;Max/Min',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=DIFFBG,font = SMALLBUTTONFONT) .grid(column=1,row=2)
+        tk.Button(f2,text='Integration;Basics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=2,row=2)
+        tk.Button(f2,text='Integration;Functions',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg=INTBG,font = SMALLBUTTONFONT) .grid(column=0,row=3)
+        tk.Button(f2,text='Integration;Kinematics',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= INTBG,font = SMALLBUTTONFONT) .grid(column=1,row=3,pady=BUTTONGAPY)
+        tk.Button(f2,text='Final Quiz',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg= "chocolate2",font = SMALLBUTTONFONT) .grid(column=2,row=3)
+        tk.Button(f2,text='Back',width=SSBUTTONWIDTH,height=SSBUTTONHEIGHT,command="",bg="orange red2",font = SMALLBUTTONFONT) .grid(column=1,row=4,pady=BACKBUTTONGAPY)
 
 def main():
     global f1,f2,root
